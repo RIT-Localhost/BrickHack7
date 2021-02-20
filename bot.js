0e022542f9c61979c7ec5580bdcd721e68a8d2de
wmackin <wmackin@live.com>
2021-02-20 17:00:20 -0500

    Add events and event reminders.

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+const { SSL_OP_NETSCAPE_REUSE_CIPHER_CHANGE_BUG } = require('constants')
 const Discord = require('discord.js')
 // const config = require('./config')
 const { title } = require('process')
@@ -8,3 +9,49 @@ discordClient.on('ready', () => {
 })
 // discordClient.login(config.discordApiToken) 
 discordClient.login(process.env.DISCORD_API_TOKEN) // make sure the key is called DISCORD_API_TOKEN
+
+discordClient.on('message', message => {
+  if (message.startsWith("!addevent")) {
+    console.log("Recognized message.")
+    addEvent(message);
+  }
+})
+
+let events = [];
+
+function addEvent(message) {
+  fields = message.split("--");
+  eventName = fields[0].split("!addevent ");
+
+  let newEvent = {};
+  newEvent.name = eventName;
+  console.log(eventName)
+  if (message.search("--t") != -1) {
+    console.log("Found time flag")
+    let time = fields.split("--t")[1].split("--")[0].strip();
+    console.log(time);
+    newEvent.time = time;
+    var eventTime = new Date(time);
+    var currentTime = new Date().getTime();
+    var offset = currentTime - eventTime;
+    setTimeout(function() {eventReminder(channel, newEvent.name)}, offset);
+
+  }
+  else {
+    newEvent.time = "";
+  }
+  if (message.search("--d") != 1) {
+    let description = fields.split("--d")[1].split("--")[0].strip();
+    newEvent.description = description;
+  }
+  else {
+    newEvent.description = "";
+  }
+  
+}
+
+
+function eventReminder(channel, eventName) {
+  console.log("Event time.");
+  channel.send("@everyone" + eventName + "HAPPENING NOW!");
+}
