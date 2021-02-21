68134a8bdf7da11b3665e18656aa4ee63c4952da
wmackin <wmackin@live.com>
2021-02-20 22:14:45 -0500

    Implemented more flag options.

--- bot.js
+++ bot.js
@@ -34,34 +34,52 @@ discordClient.on('message', message => {
 });
 
 let events = [];
-let fields = [];
 
 function addEvent(message) {
   const fields = message.toString().split(' --');
   const eventName = fields[0].split('!addevent ')[1];
+  var channel = message.channel;
 
   let newEvent = {};
+  events.push(newEvent);
   newEvent.name = eventName;
-  console.log(eventName);
-  if (message.toString().search('--d') != 1) {
-    let description = fields;
+  if (message.toString().search('--d') != -1) {
+    let description = message.content.split(' --d ')[1].split(' --')[0];
     newEvent.description = description;
   } else {
+    let description = "";
     newEvent.description = '';
   }
   if (message.content.search('--w') != -1) {
-      console.log("Found w");
-      var weekly = true;
+    console.log("Found w");
+    var weekly = true;
+    newEvent.weekly = "true";
   } else {
-      var weekly = false;
+    var weekly = false;
+    newEvent.weekly = "false";
   }
   if (message.content.search('--m') != -1) {
-      var monthly = true;
+    var monthly = true;
+    newEvent.monthly = "true";
   } else {
-      var monthly = false;
+    var monthly = false;
+    newEvent.monthly = "false";
   }
+  if (message.content.search('--c') != -1) {
+    let channelName = message.content.split(' --c ')[1].split(' --')[0];
+    channelName = channelName.substring(2, channelName.length - 1);
+    channel = discordClient.channels.cache.get(channelName);
+    newEvent.channel = channel;
+  } else {
+    newEvent.channel = "";
+  }
+  if (message.content.search('--r') != -1) {
+    var role = message.content.split(' --r ')[1].split(' --')[0];
+  } else {
+    role = "everyone";
+  }
+  newEvent.role = role;
   if (message.toString().search('--t') != -1) {
-    console.log('Found time flag');
     let time = message.toString().split(' --t ')[1].split(' --')[0];
     console.log(time);
     newEvent.time = time;
@@ -69,22 +87,44 @@ function addEvent(message) {
     let eventTime = new Date(time);
     let currentTime = new Date();
     let offset = eventTime - currentTime;
-    console.log("Event time: " + eventTime);
-    console.log("Current time: " + currentTime);
-    console.log("Offset: " + offset);
     setTimeout(function () {
-      eventReminder(message.channel, newEvent.name, weekly);
+      eventReminder(channel, newEvent.name, weekly, monthly, role);
     }, offset);
   } else {
     newEvent.time = '';
   }
 }
 
-function eventReminder(channel, eventName, weekly) {
-  console.log('Event time.');
-  channel.send('everyone ' + eventName + ' HAPPENING NOW!');
+function eventReminder(channel, eventName, weekly, monthly, role) {
+  channel.send(role + ' ' + eventName + ' HAPPENING NOW!');
   if (weekly == true) {
-    console.log("Into weekly.");
-    setInterval(function() {channel.send('everyone ' + eventName + ' HAPPENING NOW!');}, 1000 * 60 * 60 * 24 * 7);
+    setInterval(function() {channel.send(role + ' ' + eventName + ' HAPPENING NOW!');}, 1000 * 60 * 60 * 24 * 7);
+  }
+  if (monthly == true) {
+    let currentTime = new Date();
+    let year = currentTime.getFullYear();
+    let month = currentTime.getMonth();
+    let day = currentTime.getDate();
+    let hour = currentTime.getHours();
+    let minute = currentTime.getMinutes();
+    let millis = currentTime.getMilliseconds();
+    if (month == 12) {
+      year++;
+    }
+    if (month == 1 && day > 28) {
+      day = 28;
+    }
+    if (day > 30 && (month == 3 || month == 5 || month == 8 || month == 10)) {
+      day = 30;
+    }
+    month = (month % 12) + 1;
+    let eventTime = new Date(year, month, day, hour, minute, millis);
+    let offset = eventTime - currentTime;
+    console.log("Event time: " + eventTime);
+    console.log("Current time: " + currentTime);
+    console.log("Offset: " + offset);
+    setTimeout(function () {
+      eventReminder(channel, eventName, false, true, role);
+    }, offset);
   }
 }
